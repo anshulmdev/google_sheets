d95357238c24d52875feaf64660b38e3ef679e73
Anshul <anshul@aegiscovenant.com>
2024-01-29 11:59:48 +0530

    Schema Setup

--- controllers/excelConverter.js
+++ controllers/excelConverter.js
@@ -1,12 +1,34 @@
-
-const excel = require('excel4node');
-const workbook = new excel.Workbook();
-
-export const excelConverter = (tableName, tableFields, tableRecords) => {
+import writeXlsxFile from 'write-excel-file'
 
 
+const mainConverter = async (fileName, schema, dataObject) => {
+    await writeXlsxFile(dataObject, { schema, fileName: `${fileName}-file.xlsx`})
+    return
+}
+
+export const excelConverter = async (tableName, tableFields, tableRecords) => {
     try {
-        const worksheet = workbook.addWorksheet(tableName);
+        tableFields = tableFields.map((e) => e = { name: e.name, type: e.type })
+        let records;
+        tableRecords.records.forEach(element => {
+            records.push(element._data.cellValuesByFieldId)
+        });
+        const schema = []
+        const checkType = (type) => {
+            switch(type) {
+                case "createdTime": return Date ;
+                case "checkbox": return Boolean;
+                default: return String ;
+            }
+        }
+        tableFields.forEach((e) => { schema.push({column: e.name, type: checkType(e.type), value: 100})})
+        console.log('Schema', schema)
+
+
+
+
+
+        return;
         
     } catch (error) {
         console.log(error);
